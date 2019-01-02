54eb78454984a1352f31d00c7cf5927ebff53175
CJBuchel <connorbuchel@gmail.com>
2019-01-02 22:08:48 +0800

    Added header Explanation

--- header_example/header.h
+++ header_example/header.h
@@ -1,5 +1,9 @@
-_Pragma once // this is a fancy piece of code that just allows you to have connections
+#pragma once; // this is a fancy piece of code that just allows you to have connections
 // to libraries all over your computer
+
+// The reason we use headers is to split up all the programming, so it's not all running in one folder,
+// some benifits of this is glitch control. If a glitch happens in one peice of the code (but it's split up)
+// it won't affect other codes in other folders
 
 int num = 10; // this is an integer. an integer allows you to store values in a name.
 // in this case the value is 10, and the name is num.
